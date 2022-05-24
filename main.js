0a4f814aa4859a22fb8b1cc9f6c1901cee932019
Youssef Mahmoud <kamel.youssef1@gmail.com>
2022-05-24 19:34:34 +0300

    Update arrays

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-const decision_maker = ['Yes', 'No', 'Maybe', 'I can\'t say right now..', 'Definitely', 'You have to be kidding me'];
+const answer_user_question = ['Yes', 'No', 'Maybe', 'I can\'t say right now..', 'Definitely', 'You have to be kidding me'];
+const subject_pronouns = ['I', 'You', 'He', 'She', 'It', 'They'];
+const verbs = ['love', 'hate', 'like', 'adore', 'despise'];
+const object_pronouns = ['him', 'her', 'it', 'them'];
