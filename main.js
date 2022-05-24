5f487a776f497c4262ac961aa7b7739d82e7b1ad
Youssef Mahmoud <kamel.youssef1@gmail.com>
2022-05-24 21:33:29 +0300

    Tests

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const answer_user_question = ['Yes', 'No', 'Maybe', 'I can\'t say right now..', 
 const subject_pronouns = ['I', 'You', 'He', 'She', 'It', 'They'];
 const verbs = ['love', 'hate', 'like', 'adore', 'despise'];
 const object_pronouns = ['him', 'her', 'it', 'them'];
+const prompt = require('prompt-sync')();
 
 function random_words(array) {
     let word = array[Math.floor(Math.random() * array.length)];
@@ -17,12 +18,12 @@ function sentence_maker() {
 }
 
 function answer_to_your_question() {
-    const question = prompt('I will give you an answer to a question you have instead. Ask me.');
-    console.log(`${prompt}: ${random_words(answer_user_question)}.`);
+    const question = prompt('I will give you an answer to a question you have instead. Ask me. ');
+    console.log(`${question}. The answer is ${random_words(answer_user_question)}.`);
 }
 
 function in_or_out() {
-    let decision = prompt('Do you want a random fact?');
+    let decision = prompt('Do you want a random fact? ');
     let looper = false;
     while(!looper) {
         if(decision === 'Y' || decision === 'Yes') {
@@ -32,7 +33,11 @@ function in_or_out() {
             answer_to_your_question();
             looper = true;
         }else {
-            decision = prompt('I cannot understand what you said. Answer my question again.');
+            decision = prompt('I cannot understand what you said. Answer my question again. ');
         }
     }
 }
+
+//tests
+
+in_or_out();
