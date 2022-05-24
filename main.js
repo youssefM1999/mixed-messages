601b527a46d1dd67e1877dc7eed88db1a9d1891f
Youssef Mahmoud <kamel.youssef1@gmail.com>
2022-05-24 19:51:22 +0300

    Sentence maker functions

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ const answer_user_question = ['Yes', 'No', 'Maybe', 'I can\'t say right now..', 
 const subject_pronouns = ['I', 'You', 'He', 'She', 'It', 'They'];
 const verbs = ['love', 'hate', 'like', 'adore', 'despise'];
 const object_pronouns = ['him', 'her', 'it', 'them'];
+
+function random_words(array) {
+    let word = array[Math.floor(Math.random() * array.length)];
+    return word;
+}
+function sentence_maker() {
+    let first_word = random_words(subject_pronouns);
+    let second_word = random_words(verbs);
+    let third_word = random_words(object_pronouns);
+
+    const sentence = `${first_word} ${second_word} ${third_word}`;
+    return sentence;
+}
+
